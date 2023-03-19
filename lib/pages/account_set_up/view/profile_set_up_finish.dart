6884cf3ab1285f91a1cdfb6886b5eb2810a75308
piekarskipiotr <ppiekarski8@gmail.com>
2023-03-19 16:53:29 +0100

    create account set up finish page

---
+++ lib/pages/account_set_up/view/profile_set_up_finish.dart
@@ -0,0 +1,83 @@
+import 'package:betterrank/config/config.dart';
+import 'package:betterrank/l10n/l10n.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lottie/lottie.dart';
+
+class ProfileSetUpFinish extends StatelessWidget {
+  const ProfileSetUpFinish({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final l10n = context.l10n;
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: Stack(
+        children: [
+          SizedBox(
+            height: double.infinity,
+            child: Lottie.asset(
+              'assets/anim/confetti.json',
+              fit: BoxFit.cover,
+              repeat: false,
+            ),
+          ),
+          SafeArea(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 36),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(height: 16),
+                      Text(
+                        l10n.profile_finish_header,
+                        style: GoogleFonts.inter(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 36,
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+                      Opacity(
+                        opacity: 0.9,
+                        child: Text(
+                          l10n.profile_finish_header_sub,
+                          textAlign: TextAlign.left,
+                          style: GoogleFonts.inter(
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const Spacer(),
+                InkWell(
+                  onTap: () => context.go(AppRoutes.home),
+                  child: Container(
+                    width: double.infinity,
+                    padding: const EdgeInsets.symmetric(vertical: 24),
+                    child: Text(
+                      l10n.start_app,
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.inter(
+                        fontSize: 16,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
